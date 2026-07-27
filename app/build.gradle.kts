7e28e7feb25ecece2ac3156d6824ab06594bc223
Son-Vegitto <21322592+Son-Vegitto@users.noreply.github.com>
2026-07-27 16:05:02 -0400

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.invisible.popuplauncher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.invisible.popuplauncher"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Zero heavy dependencies to keep the APK < 100 KB
+    implementation("androidx.core:core-ktx:1.12.0")
+}
